1f0c4804d4a2b68ab2684b09291d91de984f1ae0
Warnigo Warfurious <119101655+Warnigo@users.noreply.github.com>
2024-11-06 21:16:02 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config():
-    API_ID = "22873973" # User api id
-    API_HASH = "78745aa209fb1f040d475caab32f5f4c" # Use api hash
-    PHONE_NUMBER = "+998889794414" # User phone number
-    NAME = "telegram-chanel" # Options name
-    SRC_CHAT_ID = -1002477981126 # For CHANEL id
-    DEST_CHAT_ID = -1002262481591 # For GROUP id
